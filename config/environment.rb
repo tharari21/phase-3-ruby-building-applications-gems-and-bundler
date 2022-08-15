a08661624de169250604393523d8833042da4c80
Tomer Harari <tharari21@gmail.com>
2022-08-15 10:52:24 -0400

    commit tree 0e526fc56421c0c2c05b7c516a86c23adb431001

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,4 @@
 # Set up Bundler here following the instructions in the README
+require 'bundler/setup'
+# require takes a list of groups to load in to the file. The default group is everything in the file that is not a group
+Bundler.require(:default, :development)
